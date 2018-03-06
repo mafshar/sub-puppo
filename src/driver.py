d3fd5ef3f133828207538916e342f27dd1ee1d41
mafshar <ma2510@nyu.edu>
2018-03-05 20:21:26 -0500

    added mfcc feature calculation

--- src/driver.py
+++ src/driver.py
@@ -5,6 +5,7 @@ Notes:
     - Weak implies weakly supervised learning (4 classes)
     - Strong implies strongly (fully) superversied learning (10 classes)
     - frame number is set to 20ms (default); that is the "sweet spot" based on dsp literature
+    - sampling rate is 16kHz (for the MFCC of each track)
 '''
 
 import os
@@ -17,10 +18,14 @@ from librosa import load
 from librosa import feature
 from collections import defaultdict
 
-GENRES = ['pop', 'rock', 'hiphop', 'jazz', 'metal', 'disco', 'blues', 'reggae', 'country', 'classical']
-CALCULATED_MFCCS = True
+genres = ['pop', 'rock', 'hiphop', 'jazz', 'metal', 'disco', 'blues', 'reggae', 'country', 'classical']
+have_mfccs = True
 
-def calc_mfccs(input_path, output_path, verbose=False):
+def calc_mfcc(file):
+    y, sr = load(file, sr=16050)
+    return feature.mfcc(y=y, sr=sr, n_mfcc=22)
+
+def write_mfccs(input_path, output_path, verbose=False):
     mfccs = defaultdict(lambda: [])
     for sub in os.listdir(input_path):
         input_dir = os.path.join(input_path, sub)
@@ -33,8 +38,8 @@ def calc_mfccs(input_path, output_path, verbose=False):
         for input_file in glob.glob(os.path.join(input_dir, '*.au')):
             count = os.path.basename(input_file).split('.')[1]
             output_file = os.path.join(output_dir, sub + '_' + count + '_mfcc.npy')
-            y, sr = load(input_file)
-            mfcc = feature.mfcc(y=y, sr=sr)
+            mfcc = calc_mfcc(input_file)
+            print mfcc.shape
             mfccs[sub].append(mfcc)
             np.save(file=output_file, arr=mfcc)
         toc = time.time()
@@ -42,7 +47,7 @@ def calc_mfccs(input_path, output_path, verbose=False):
             print '\ttime it took to obtain mfccs for', sub, 'is', toc-tic
     return mfccs
 
-def retrieve_mfccs(path, verbose=True):
+def read_mfccs(path, verbose=False):
     mfccs = defaultdict(lambda: [])
     for sub in os.listdir(path):
         dir = os.path.join(path, sub)
@@ -59,19 +64,34 @@ def retrieve_mfccs(path, verbose=True):
             print '\ttime it took to retrieve mfccs for', sub, 'is', toc-tic
     return mfccs
 
+def calc_features(mfccs, verbose=False):
+    features = defaultdict(lambda: [])
+    for genre in mfccs:
+        tic = time.time()
+        if verbose:
+            print 'calculating mfcc features for', genre
+        for mfcc in mfccs[genre]:
+            features[genre].append(np.sum(mfcc, axis=1, keepdims=True))
+        toc = time.time()
+        if verbose:
+            print '\ttime taken to calculate mfcc features for', genre, 'is', toc-tic
+    return features
+
 if __name__ == '__main__':
     input_path = './data/genres/'
-    mfcc_path = './data/processed/mfcc'
+    mfcc_path = './data/processed/mfcc/'
     mfccs = None
-    if not CALCULATED_MFCCS:
+    features = None
+    if not have_mfccs:
+        have_mfccs = True
         print 'calculating mfccs...'
-        mfccs = calc_mfccs(input_path, mfcc_path, True)
+        mfccs = write_mfccs(input_path, mfcc_path, True)
     else :
         print 'retrieving mfccs...'
-        mfccs = retrieve_mfccs(mfcc_path)
+        mfccs = read_mfccs(mfcc_path, True)
 
-    ## they are of different sizes!!!
-    #@TODO: come up with a way to allow for learning given a set of mfccs
-    for genre in mfccs:
-        for coeffs in mfccs[genre]:
-            print coeffs.shape
+    features = calc_features(mfccs, True)
+    for genre in features:
+        for mfcc in features[genre]:
+            print mfcc.shape
+    
